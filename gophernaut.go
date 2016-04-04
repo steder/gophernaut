dfadd277e98c1b8fe0d60ca3237b61ac89d470c9
Mike Steder <steder@gmail.com>
2016-04-03 23:40:39 -0500

    Witness the power of this barely-operational battle station

--- gophernaut.go
+++ gophernaut.go
@@ -1,21 +1,33 @@
 package main
 
-import "os/exec"
 import "fmt"
+import "net/http"
+import "net/http/httputil"
+import "net/url"
+import "os/exec"
 import "strings"
 
-var executable string = "python -m SimpleHTTPServer 8080"
+var hostname = fmt.Sprintf("http://127.0.0.1:%d", 8080)
+var executable string = fmt.Sprintf("python -m SimpleHTTPServer %d", 8080)
 
-func start_process() {
+func start_process(events chan int) {
 	command_parts := strings.Split(executable, " ")
 	command := exec.Command(command_parts[0], command_parts[1:]...)
 	fmt.Printf("Command: %v\n", command)
 	command.Start()
+	_, ok := <-events
+	if !ok {
+		command.Process.Kill()
+	}
 }
 
 func main() {
-	go start_process()
+	events_channel := make(chan int)
+	go start_process(events_channel)
 	fmt.Printf("Gophernaut is ready for eBusiness!\n")
-	for {
-	}
+	hostname_url, _ := url.Parse(hostname)
+	proxy := httputil.NewSingleHostReverseProxy(hostname_url)
+	// Time to listen for connections!
+	http.ListenAndServe(":8483", proxy)
+
 }
