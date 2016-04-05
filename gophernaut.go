e89b6a1d037cb1ca3e95a1fdf0035a581f58249b
Mike Steder <steder@gmail.com>
2016-04-04 22:16:25 -0500

    Add admin interface and hook http handlers up in the right order

--- gophernaut.go
+++ gophernaut.go
@@ -1,10 +1,12 @@
 package main
 
 import "fmt"
+import "html/template"
 import "net/http"
 import "net/http/httputil"
 import "net/url"
 import "os/exec"
+import "path"
 import "strings"
 
 var hostname = fmt.Sprintf("http://127.0.0.1:%d", 8080)
@@ -21,13 +23,51 @@ func start_process(events chan int) {
 	}
 }
 
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	request_path := req.URL.Path
+
+	target_url, _ := url.Parse(hostname)
+	director := func(req *http.Request) {
+		targetQuery := target_url.RawQuery
+		req.URL.Scheme = target_url.Scheme
+		// TODO: adjust request host to assign the request to the appropriate child process
+		req.URL.Host = target_url.Host
+		req.URL.Path = path.Join(target_url.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	static_handler := http.StripPrefix("/static", http.FileServer(http.Dir("static")))
+	admin_template := template.Must(template.ParseFiles("templates/admin.html"))
+	admin_handler := func(w http.ResponseWriter, req *http.Request) {
+		admin_template.Execute(w, nil)
+	}
+
+	fmt.Printf("path: %s\n", request_path)
+	switch {
+	case request_path == "/admin":
+		fmt.Printf("admin path...\n")
+		admin_handler(w, req)
+		return
+	case strings.HasPrefix(request_path, "/static"):
+		fmt.Printf("static path...\n")
+		static_handler.ServeHTTP(w, req)
+		return
+	}
+	fmt.Printf("proxy path...\n")
+	proxy.ServeHTTP(w, req)
+}
+
 func main() {
 	events_channel := make(chan int)
 	go start_process(events_channel)
 	fmt.Printf("Gophernaut is ready for eBusiness!\n")
-	hostname_url, _ := url.Parse(hostname)
-	proxy := httputil.NewSingleHostReverseProxy(hostname_url)
-	// Time to listen for connections!
-	http.ListenAndServe(":8483", proxy)
-
+	http.ListenAndServe(":8483", http.HandlerFunc(myHandler))
+	// TODO: our own ReverseProxy implementation of at least, ServeHTTP so that we can
+	// monitor the response codes to track successes and failures
 }
