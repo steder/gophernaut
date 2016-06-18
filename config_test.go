3957bda78f4d83a9f7548631e70928b09a251e52
Mike Steder <steder@gmail.com>
2016-06-18 10:06:54 -0500

    adding some tests

--- config_test.go
+++ config_test.go
@@ -3,5 +3,13 @@ package gophernaut
 import "testing"
 
 func TestConfig(t *testing.T) {
+	t.Log(Config{
+		Host: "foo.bar",
+		Port: 1234,
+	})
+}
 
+func TestRead(t *testing.T) {
+	c := ReadConfig()
+	t.Log(c)
 }
