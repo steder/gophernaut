087eee18a5a7165be0653ce48f5e670e6adc83d6
Mike Steder <steder@gmail.com>
2016-06-18 23:58:46 -0500

    Moved handler into its own file

--- cmd/gophernaut/main.go
+++ cmd/gophernaut/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"path"
-	"strings"
 
 	"github.com/steder/gophernaut"
 )
@@ -21,54 +16,6 @@ var hostnames = []string{
 var executables = []string{
 	fmt.Sprintf("python -m SimpleHTTPServer %d", 8080),
 	fmt.Sprintf("python -m SimpleHTTPServer %d", 8081),
-}
-
-var requestCount = 0
-
-func myHandler(w http.ResponseWriter, myReq *http.Request) {
-	requestPath := myReq.URL.Path
-	// DONE: adjust request host to assign the request to the appropriate child process
-	// TODO: multiprocess, pick one of n hostnames based on pool status
-	hostname := hostnames[requestCount%len(hostnames)]
-	requestCount++
-	targetURL, _ := url.Parse(hostname)
-	director := func(req *http.Request) {
-		targetQuery := targetURL.RawQuery
-		req.URL.Scheme = targetURL.Scheme
-		req.URL.Host = targetURL.Host
-
-		// clean up but preserve trailing slash:
-		trailing := strings.HasSuffix(req.URL.Path, "/")
-		req.URL.Path = path.Join(targetURL.Path, req.URL.Path)
-		if trailing && !strings.HasSuffix(req.URL.Path, "/") {
-			req.URL.Path += "/"
-		}
-
-		// preserve query string:
-		if targetQuery == "" || req.URL.RawQuery == "" {
-			req.URL.RawQuery = targetQuery + req.URL.RawQuery
-		} else {
-			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-		}
-	}
-
-	proxy := &httputil.ReverseProxy{Director: director}
-
-	staticHandler := http.StripPrefix("/static", http.FileServer(http.Dir("static")))
-	adminTemplate := template.Must(template.ParseFiles("templates/admin.html"))
-	adminHandler := func(w http.ResponseWriter, req *http.Request) {
-		adminTemplate.Execute(w, nil)
-	}
-
-	switch {
-	case requestPath == "/admin":
-		adminHandler(w, myReq)
-		return
-	case strings.HasPrefix(requestPath, "/static"):
-		staticHandler.ServeHTTP(w, myReq)
-		return
-	}
-	proxy.ServeHTTP(w, myReq)
 }
 
 func main() {
@@ -84,5 +31,5 @@ func main() {
 	log.Printf("Gophernaut is gopher launch!\n")
 	// TODO: our own ReverseProxy implementation of at least, ServeHTTP so that we can
 	// monitor the response codes to track successes and failures
-	log.Fatal(http.ListenAndServe(":8483", http.HandlerFunc(myHandler)))
+	log.Fatal(http.ListenAndServe(":8483", http.HandlerFunc(gophernaut.GetGopherHandler(hostnames))))
 }
---
+++ handlers.go
@@ -0,0 +1,63 @@
+package gophernaut
+
+import (
+	"html/template"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"path"
+	"strings"
+)
+
+// GetGopherHandler ...
+func GetGopherHandler(hostnames []string) func(w http.ResponseWriter, r *http.Request) {
+	var requestCount = 0
+
+	myHandler := func(responseWriter http.ResponseWriter, myReq *http.Request) {
+		requestPath := myReq.URL.Path
+		// DONE: adjust request host to assign the request to the appropriate child process
+		// TODO: multiprocess, pick one of n hostnames based on pool status
+		hostname := hostnames[requestCount%len(hostnames)]
+		requestCount++
+		targetURL, _ := url.Parse(hostname)
+		director := func(req *http.Request) {
+			targetQuery := targetURL.RawQuery
+			req.URL.Scheme = targetURL.Scheme
+			req.URL.Host = targetURL.Host
+
+			// clean up but preserve trailing slash:
+			trailing := strings.HasSuffix(req.URL.Path, "/")
+			req.URL.Path = path.Join(targetURL.Path, req.URL.Path)
+			if trailing && !strings.HasSuffix(req.URL.Path, "/") {
+				req.URL.Path += "/"
+			}
+
+			// preserve query string:
+			if targetQuery == "" || req.URL.RawQuery == "" {
+				req.URL.RawQuery = targetQuery + req.URL.RawQuery
+			} else {
+				req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+			}
+		}
+
+		proxy := &httputil.ReverseProxy{Director: director}
+
+		staticHandler := http.StripPrefix("/static", http.FileServer(http.Dir("static")))
+		adminTemplate := template.Must(template.ParseFiles("templates/admin.html"))
+		adminHandler := func(w http.ResponseWriter, req *http.Request) {
+			adminTemplate.Execute(w, nil)
+		}
+
+		switch {
+		case requestPath == "/admin":
+			adminHandler(responseWriter, myReq)
+			return
+		case strings.HasPrefix(requestPath, "/static"):
+			staticHandler.ServeHTTP(responseWriter, myReq)
+			return
+		}
+		proxy.ServeHTTP(responseWriter, myReq)
+	}
+
+	return myHandler
+}
