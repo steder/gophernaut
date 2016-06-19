7297d510c1a0fd0c1e3b9d300ba5f7a97dfa1479
Mike Steder <steder@gmail.com>
2016-06-19 00:45:55 -0500

    dynamically figure out number of processes

--- cmd/gophernaut/main.go
+++ cmd/gophernaut/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,6 +13,12 @@ func main() {
 	log.SetFlags(log.Ldate | log.Ltime)
 	c := gophernaut.ReadConfig()
 	log.Printf("Host %s and Port %d\n", c.Host, c.Port)
+	log.Printf("Pool %d, %s, %s, %s\n",
+		c.Pool.Size,
+		c.Pool.Template.Name,
+		c.Pool.Template.Executable,
+		c.Pool.Template.Hostname,
+	)
 
 	pool := gophernaut.Pool{Executables: c.GetExecutables()}
 	pool.Start()
@@ -20,6 +27,6 @@ func main() {
 	log.Printf("Gophernaut is gopher launch!\n")
 	// TODO: our own ReverseProxy implementation of at least, ServeHTTP so that we can
 	// monitor the response codes to track successes and failures
-	log.Fatal(http.ListenAndServe(":8483",
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Port),
 		http.HandlerFunc(gophernaut.GetGopherHandler(c.GetHostnames()))))
 }
--- config.go
+++ config.go
@@ -13,15 +13,14 @@ Config is a Gophernaught config structure used to parse gophernaut.conf
 type Config struct {
 	Host string
 	Port int
-	/*
-	    	Debug bool
-	   	Pool struct {
-	   		Size int
-	   		Template struct {
-	   			Name string
-	   		}
-	   	}
-	*/
+	Pool struct {
+		Size     int
+		Template struct {
+			Name       string
+			Hostname   string
+			Executable string
+		}
+	}
 }
 
 func check(e error) {
@@ -43,18 +42,20 @@ func ReadConfig() *Config {
 
 // GetExecutables uses our config to provide a set of processes to start
 func (c *Config) GetExecutables() []string {
-	var executables = []string{
-		fmt.Sprintf("python -m SimpleHTTPServer %d", 8080),
-		fmt.Sprintf("python -m SimpleHTTPServer %d", 8081),
+	var executables []string
+	for x := 0; x < c.Pool.Size; x++ {
+		executables = append(executables,
+			fmt.Sprintf("python -m SimpleHTTPServer %d", 8080+x))
 	}
 	return executables
 }
 
 // GetHostnames uses our config to provide a set of hostnames to dispatch requests to
 func (c *Config) GetHostnames() []string {
-	var hostnames = []string{
-		fmt.Sprintf("http://127.0.0.1:%d", 8080),
-		fmt.Sprintf("http://127.0.0.1:%d", 8081),
+	var hostnames []string
+	for x := 0; x < c.Pool.Size; x++ {
+		hostnames = append(hostnames,
+			fmt.Sprintf("http://127.0.0.1:%d", 8080+x))
 	}
 	return hostnames
 }
